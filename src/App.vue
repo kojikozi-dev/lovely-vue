869d8923b168738ebf6f2ec1bb6ddd18d4244ce7
kouhei-kojima <kouhei_kojima@waku-2.com>
2019-06-28 10:53:57 +0900

    5:Loop

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,13 @@
   <div id="app">
     <h1>Lovely Vue</h1>
     <input v-model="message" />
+    <button @click="add">追加</button>
     <br />
+    <ul>
+      <li v-for="todo in todos" :key="todo.id" class="task-card">
+        {{ todo.text }}
+      </li>
+    </ul>
     {{ message }}
   </div>
 </template>
@@ -11,9 +17,27 @@
 export default {
   name: "App",
   data: () => ({
+    todos: [{ id: 1, text: "ときめき" }],
     message: "Spark joy!"
-  })
+  }),
+  methods: {
+    add() {
+      const newTodo = {
+        id: this.todos.length + 1,
+        text: this.message
+      };
+      this.todos.push(newTodo);
+      this.message = "";
+    }
+  }
 };
 </script>
 
-<style></style>
+<style>
+.task-card {
+  border: solid 1px;
+  margin: 5px;
+  padding: 2px;
+  width: 250px;
+}
+</style>
